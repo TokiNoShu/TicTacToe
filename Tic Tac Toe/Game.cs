e3d0eee133d4797a66212202fd676771544f3419
TokiNoShu <egormagerov11@gmail.com>
2025-01-17 10:31:33 +0300

    Add files via upload

---
+++ Tic Tac Toe/Game.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tic_Tac_Toe
+{
+    public class Game
+    {
+        private Board board;
+        private Player player1;
+        private Player player2;
+        private Player currentPlayer;
+
+        public Game()
+        {
+            board = new Board();
+            player1 = new Player('X');
+            player2 = new Player('O');
+            currentPlayer = player1;
+        }
+        public void Start()
+        {
+            while (true)
+            {
+                Console.Clear();
+                board.Display();
+                Console.WriteLine($"Ход игрока {currentPlayer.Symbol}. Введите номер клетки (1-9): ");
+                int cell;
+                if (int.TryParse(Console.ReadLine(), out cell) && board.IsCellAvailable(cell))
+                {
+                    board.MarkCell(cell, currentPlayer.Symbol);
+
+                    if (board.CheckWin(currentPlayer.Symbol))
+                    {
+                        Console.Clear();
+                        board.Display();
+                        Console.WriteLine($"Игрок {currentPlayer.Symbol} выйграл!");
+                        break;
+                    }
+                    else if (board.IsFool())
+                    {
+                        Console.Clear();
+                        board.Display();
+                        Console.WriteLine("Ничья!");
+                        break;
+                    }
+                    currentPlayer = (currentPlayer == player1) ? player2 : player1;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректный ввод. Попробуйте снова");
+                }
+            }
+        }
+    }
+}
---
+++ Tic Tac Toe/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Tic_Tac_Toe
+{
+    class Program
+    {    
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+            game.Start();
+        }
+    }
+
+}
